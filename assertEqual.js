c51c8c773ceebc1acdc284b86800b47ac4089fd9
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-02 13:59:45 -0500

    finished assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('💪️💪️💪️ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('😿️😿️😿️ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(1, '1');
