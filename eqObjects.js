ab6d78fad6a77e6c7d6ed0c82541f25ca494abed
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-06 13:03:34 -0500

    implemented eqObjects to handle nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -25,26 +25,32 @@ const eqArrays = function(a1, a2) {
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
 
-  obj1keys = Object.keys(object1);
-  obj2keys = Object.keys(object2);
+  const obj1keys = Object.keys(object1);
+  const obj2keys = Object.keys(object2);
 
   // Check if number of keys are the same
   if (obj1keys.length !== obj2keys.length)
     return false;
 
   // Go through each key in obj1 and see if values match for key in obj2
-  for (key of obj1keys) {
+  for (const key of obj1keys) {
 
     // If current value is array, use eqArrays to compare instead
     if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key]))
-        return false;
+      return eqArrays(object1[key], object2[key]);
     }
-    else if (object1[key] !== object2[key])
+
+    // If current value is object but not array, call function recursively
+    if (typeof object1[key] == 'object') {
+      return eqObjects(object1[key], object2[key]);
+    }
+
+    // Value must be primitive, so compare them directly
+    else if (object1[key] !== object2[key]) {
       return false;
+    }
   }
   return true;
-
 };
 
 // Test code for primitives\
@@ -60,5 +66,11 @@ const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd2 = { c: "1", d: ["2", 4, 4, 5] };
 assertEqual(eqObjects(cd, cd2), false);
+
+// Test code for nested objects
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1, y: 0 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
