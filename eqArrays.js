e1d8c72420621990556b5d7eb83032fdc69b2480
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-06 13:13:24 -0500

    implemented eqArrays to handle nested

--- eqArrays.js
+++ eqArrays.js
@@ -6,8 +6,15 @@ const eqArrays = function(a1, a2) {
 
   // Go through each item in array and compare the objects
   for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i])
+
+    // If elements are arrays, call eqArray function recursively
+    if (Array.isArray(a1[i]) && Array.isArray(a2[i])) {
+      if (!eqArrays(a1[i], a2[i])) {
+        return false;
+      }
+    } else if (a1[i] !== a2[i]) {
       return false;
+    }
   }
   return true;
 }
@@ -29,3 +36,9 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// Test code for nested
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
