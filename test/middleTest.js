ed3ba080c2011bcf03601dc47ab68f36cd789451
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-09 13:07:33 -0400

    refactored middle and its test

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+assertArraysEqual = require('../assertArraysEqual');
+middle = require('../middle');
+
+// Test cases
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
