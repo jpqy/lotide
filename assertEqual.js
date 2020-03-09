de7831b8601bc83ad6898dab2e46a834367d8f2c
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-09 12:22:29 -0400

    refactor assertEquals and heads into modules/test

--- assertEqual.js
+++ assertEqual.js
@@ -2,15 +2,12 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log(`💪️💪️💪️ Assertion Passed: ${actual}  ===  ${expected}`);
+    console.log(`💪️💪️💪️ Assertion Passed: ${actual}  ===  ${expected}`)
   } else {
-    console.log(`😿️😿️😿️ Assertion Failed: ${actual}  !==  ${expected}`);
+    console.log(`😿️😿️😿️ Assertion Failed: ${actual}  !==  ${expected}`)
   }
 
-};
+}
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual(1, '1');
+
+module.exports = assertEqual
