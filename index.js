0cfbbd9049d838d6a0f1b024f13c8dc2304a8e03
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-14 23:08:57 -0400

    fixed import in index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // index.js
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('assertObjectsEqual');
-const countLetters = require('countLetters');
-const countOnly = require('countOnly');
-const eqArrays = require('eqArrays');
-const eqObjects = require('eqObjects');
-const findKey = require('findKey');
-const findKeyByValue = require('findKeyByValue');
-const flatten = require('flatten');
-const head = require('head');
-const letterPosition = require('letterPosition');
-const map = require('map');
-const middle = require('middle');
-const tail = require('tail');
-const takeUntil = require('takeUntil');
-const without = require('without');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 
 
